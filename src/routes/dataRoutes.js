c62cec04e8bcb48056861be7ab8192ef9f8fa31c
christale <christalebu@gmail.com>
2022-01-15 22:55:23 -0800

    datachecker

--- src/routes/dataRoutes.js
+++ src/routes/dataRoutes.js
@@ -1,5 +1,6 @@
 import express from "express"; 
 import dataController from "../controller/dataController"
+// import dataChecker from "../middleware/datachecker";
 const dataRouter = express.Router();
 
 dataRouter.post("/house", dataController.createInfos);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import UserController from "../controller/userController";
+import dataChecker from "../middleware/datachecker";
 import express from "express";
 const userRouter = express.Router();
 
-userRouter.post("/register", UserController.createUser);
+userRouter.post("/register",dataChecker.isEmailExist,UserController.createUser);
 userRouter.get("/getusers", UserController.getAllUsers);
 userRouter.get("/:id", UserController.getOneUsers);
 userRouter.delete("/:id", UserController.deletOneUser);
